5513186266540e23ae0fdcc8c2583f028b4f20df
Mustufain <abbasmustufain@gmail.com>
2017-03-31 11:58:16 +0300

    Forward Selection Updated

--- A1/q3.py
+++ A1/q3.py
@@ -18,6 +18,7 @@ def NearestNeighbor(face_i,data,index,n_features):
 
 
             sse=(i,np.sum((row[n_features]-data[i][n_features])**2))
+            #sse=(i,np.sum((row[n_features]-data[i][n_features])**2))
             SSE.append(sse)
 
 
@@ -57,10 +58,12 @@ def ForwardSelection(Features,data):
     #Increase features uptil there is no change in SSE
     #Features 256
 
+    temp_chosen_feature=[]
     error_list=[]
     chosen_feature=[]
     Feature_count=[]
     tempset=[]
+    count=0
     for i in range(Features):
         Feature_count.append(i)
 
@@ -83,57 +86,32 @@ def ForwardSelection(Features,data):
                 total = total + error[1]
                 index+=1
 
-            tempset=[]
 
             total=total/100
             e=(k,total)
             error_list.append(e)
-
+            tempset=[]
+        count+=1
         min_sse = min(error_list, key=lambda error: error[1]) #we get a tuple (feature,sse)
-
         chosen_feature.append(min_sse[0])
-
+        m=(chosen_feature,min_sse[1])
+        temp_chosen_feature.append(m)
         Feature_count.remove(min_sse[0])
         error_list=[]
-        print min_sse
-        break
 
 
+    best_feature = min(temp_chosen_feature, key=lambda sse: sse[1])  # Feature set with min SSE
+    print best_feature
+    Feature_set = list(zip(*temp_chosen_feature)[0])
+    F = [i for i in range(0,256) ]
+    SSE = list(zip(*temp_chosen_feature))[1]
 
-
-
-        #Feature_set.append(min_sse[0])
-        #Features.remove(min_sse)
-
-        #Find minimum error and which feature also
-
-        #Feature_set=[]
-        #col_var = np.var(data[:,i])
-        #v = (i,col_var)
-        #var.append(v)
-
-
-
- #   sorted_var = sorted(var, key=lambda variance: variance[1])
-
-    #for i in range(len(var)): #first column is of maximum variance
-
-        #Feature_set.append(var[i][0])
-        #index=0
-        #total=0
-        #for face_i in data:
-
-            #SSD =  NearestNeighbor(face_i,data,index,Feature_set)
-            #target_pred = Predict(SSD,data,index)
-            #error = Sum_Of_Squared_Differences(target_pred)
-            #total = total + error[1]
-            #index+=1
-        #error_list.append(total/100)
-        #print Feature_set
-        #Feature_set=[]
+    plt.xlabel("Features")
+    plt.ylabel("SSE")
+    plt.plot(F,SSE)
+    plt.show()
     return error_list
 
-        #print (Feature_set,len(Feature_set),total/100)
 
 def Visualization(error_list):
 
@@ -153,16 +131,15 @@ def Main():
     total=0
     #for face_i in data:
 
-        #SSD = NearestNeighbor(face_i,data,index,256)
+        #SSD = NearestNeighbor(face_i,data,index,[x for x in range(0,256)])
         #target_pred=Predict(SSD,data,index)
         #error = Sum_Of_Squared_Differences(target_pred)
         #total = total + error[1]
         #index += 1
 
-    error_list = ForwardSelection(256,data)
+    #error_list = ForwardSelection(256,data)
     #Visualization(error_list)
-
-   # print (total/100)
+    #print (total/100)
 
 
 
